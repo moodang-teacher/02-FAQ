85d547a2d0602f0f22edea5cf7213f179e062e25
moodang <yssmi@naver.com>
2023-11-30 14:59:29 +0900

    질문 영역을 토글 처리

--- 02/js/script.js
+++ 02/js/script.js
@@ -6,15 +6,10 @@ $(function () {
 
   // 질문을 클릭했을 때
   $faqQ.on('click', function () {
-    // 모두에 들어간 on 클래스 제거하고
-    $faqQ.removeClass('on');
-    // 선택한 놈만 on 클래스 부여
-    $(this).addClass('on');
+    const selectQ = $(this).toggleClass('on');
+    const selectA = $(this).find($faqA).stop().slideToggle(duration);
 
-    // 선택된 놈의 답변을 찾아서 내리자
-    const selectItem = $(this).find($faqA).stop().slideDown(duration);
-
-    // 선택되지 않은(slide down되지 않은) 답변을 올리자
-    $faqA.not(selectItem).stop().slideUp(duration);
+    $faqQ.not(selectQ).removeClass('on');
+    $faqA.not(selectA).stop().slideUp(duration);
   });
 });
